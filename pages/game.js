bfa8bdf4fee0c24fe997fcb9e7efac66f6540070
Osvaldo <osvarz.new@gmail.com>
2021-06-12 15:24:28 -0500

    🔧 configuration in global styles

--- pages/game.js
+++ pages/game.js
@@ -1,7 +1,17 @@
 export default function Juego() {
   return (
-    <div>
-      <p>JUEGO</p>
-    </div>
+    <section>
+      <div className="pt-8 pr-5 flex items-center justify-end">
+        <h2>Organiza los residuos</h2>
+        <p className="w-20 h-20 rounded-full border-green-dark text-green-dark">
+          1:00
+        </p>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </section>
   )
 }
